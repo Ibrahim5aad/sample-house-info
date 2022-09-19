4587a57324962eec419be6d1919cd4167fd618e3
Ibrahim Saad <ibrahimsaad419@gmail.com>
2022-09-19 16:57:49 +0200

    feat: add first client (console app)

---
+++ src/Clients/ConsoleClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using IdentityModel.Client;
+using System.Net.Http.Headers;
+
+using var identityServerHttpClient = new HttpClient
+{
+  BaseAddress = new Uri(Environment.GetEnvironmentVariable("AUTHENTICATION__AUTHORITY")!)
+
+};
+
+
+var discoveryDocument = await identityServerHttpClient.GetDiscoveryDocumentAsync();
+
+Console.WriteLine(discoveryDocument.TokenEndpoint);
+
+var tokenResponse = await identityServerHttpClient.RequestClientCredentialsTokenAsync
+                        (new ClientCredentialsTokenRequest
+                        {
+                          Address = discoveryDocument.TokenEndpoint,
+                          ClientId = Environment.GetEnvironmentVariable("AUTHENTICATION__CLIENTID")!,
+                          ClientSecret = Environment.GetEnvironmentVariable("AUTHENTICATION__CLIENTSECRET")!,
+                          Scope = "api",
+                        });
+
+Console.WriteLine(tokenResponse.AccessToken);
+
+using var httpClient = new HttpClient()
+{
+  DefaultRequestHeaders =
+  {
+    Authorization = new AuthenticationHeaderValue(scheme: "Bearer", tokenResponse.AccessToken)
+  },
+  BaseAddress = new Uri("https://api:7001/api/")
+};
+
+Console.WriteLine(await httpClient.GetStringAsync("rooms"));
